0f1f156e848caf04a5005667dde1e33ecda7d2fa
Muzaffer <muzzusyed153@gmail.com>
2022-12-29 17:53:31 +0530

    Add comments to explain getComputerChoice function

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,10 @@
+// displays a random choice out of the three
 function getComputerChoice() {
+
+    // the randomNumber gives any one number between 0-2 
     let randomNumber = Math.round(Math.random() * 2);
+
+    // switch converts the number to choice accordingly
     switch (randomNumber) {
         case 0:
             console.log("Rock");
@@ -15,3 +20,4 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
