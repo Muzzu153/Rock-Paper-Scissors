7523c03aea49d2966b57d322a0fab6946c7aabaa
Muzaffer <muzzusyed153@gmail.com>
2023-01-01 22:42:47 +0530

    Add a function that displays the rounds result
    This function compares the user's input and the computer' choice and accordingly displays the winner

--- javascript/main.js
+++ javascript/main.js
@@ -5,19 +5,91 @@ function getComputerChoice() {
     let randomNumber = Math.round(Math.random() * 2);
 
     // switch converts the number to choice accordingly
+    let choice;
     switch (randomNumber) {
         case 0:
-            console.log("Rock");
+            choice = `Rock`;
             break;
 
         case 1:
-            console.log("Paper");
+            choice = `Paper`;
             break;
         case 2:
-            console.log("Scissors");
+            choice = `Scissors`;
             break;
     }
+    return choice;
 }
 
-getComputerChoice();
+
+//Compares the result of getComputerChoice and  users input
+function playRound() {
+
+    //prompt asks user to enter a choice between the three
+    const playerSelection = prompt("Enter your choice", " ")  //first we store it in
+    const toLowerP1ayerSelection = playerSelection.toLowerCase(); //converts choice entered by user to Lower case for comparison
+
+    //stores the result of getComputerChoice]
+    const computerSelection = getComputerChoice();
+
+    // store and display the games result
+    let result;
+
+    // conditional statements that compares user input and computer's choice
+    if (toLowerP1ayerSelection === `rock` && computerSelection === `Scissors`) {
+        console.log("Computer's choice:- Scissors");
+        result = `You won! Rock beats Scissors, good job`;
+    }
+    else if (toLowerP1ayerSelection === `paper` && computerSelection === `Rock`) {
+        console.log("Computer's choice:- Rock");
+        result = `You won! Paper beats Rock good job`;
+    }
+
+    else if (toLowerP1ayerSelection === `scissors` && computerSelection === `Paper`) {
+        console.log("Computer's choice:- Paper");
+        result = `You won! Scissors beat Paper, goof job `;
+    }
+
+
+
+    else if (toLowerP1ayerSelection === `rock` && computerSelection === `Rock`) {
+        console.log("Computer's choice:- Rock");
+        result = `It's a tie, there's still chances remaining`;
+    }
+    else if (toLowerP1ayerSelection === `paper` && computerSelection === `Paper`) {
+        console.log("Computer's choice:- Paper");
+        result = `It's a tie, there's still chances remaining`;
+    }
+    else if (toLowerP1ayerSelection === `scissors` && computerSelection === `Scissors`) {
+        console.log("Scissors");
+        result = `It's a tie, there's still chances remaining`;
+    }
+
+
+
+    else if (toLowerP1ayerSelection === `rock` && computerSelection === `Paper`) {
+        console.log("Computer's choice:- issors");
+        result = `You lose! Scissors beats Rock`;
+    }
+    else if (toLowerP1ayerSelection === `scissors` && computerSelection === `Rock`) {
+        console.log("Computer's choice:- Rock");
+        result = `You Lose! Rock beats Scissors`;
+    }
+    else if (toLowerP1ayerSelection === `paper` && computerSelection === `scissors`) {
+        console.log("Computer's choice:- Paper");
+        result = `You Lose! Rock beats Scissors`;
+    }
+    
+
+
+    else {
+        result = `Enter a valid option!!`;
+    }
+
+
+    return result;
+}
+
+
+
 
