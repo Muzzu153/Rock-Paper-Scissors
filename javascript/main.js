4ef2ae488da2c8f3df68a45e061fbac8c0ff62e4
Muzaffer <muzzusyed153@gmail.com>
2023-01-02 23:07:00 +0530

    Add the main function that starts the game
    This function calls the playRound() function and that resets after either the player or computer reaches the score of 5 and displays the winner

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,11 @@
+let computerScore = 0;
+let playerScore = 0;
+
+
+// store and display the games result
+let result = " ";
+
+
 // displays a random choice out of the three
 function getComputerChoice() {
 
@@ -32,33 +40,32 @@ function playRound() {
     //stores the result of getComputerChoice]
     const computerSelection = getComputerChoice();
 
-    // store and display the games result
-    let result;
-
     // conditional statements that compares user input and computer's choice
-    if ((toLowerP1ayerSelection === `rock` && computerSelection === `scissors`) || 
-        (toLowerP1ayerSelection === `paper` && computerSelection === `rock`) || 
+    if ((toLowerP1ayerSelection === `rock` && computerSelection === `scissors`) ||
+        (toLowerP1ayerSelection === `paper` && computerSelection === `rock`) ||
         (toLowerP1ayerSelection === `scissors` && computerSelection === `paper`)) {
 
-    console.log(`Computer's choice:- ${computerSelection}`);
-     result = `You won! ${toLowerP1ayerSelection} beats ${computerSelection} good job`;
+        console.log(`Computer's choice:- ${computerSelection}`);
+        result = `You won! ${toLowerP1ayerSelection} beats ${computerSelection} good job`;
+        console.log(`Score: Player = ${playerScore += 1}, Computer = ${computerScore}`);
     }
-    
 
 
     else if (toLowerP1ayerSelection === computerSelection) {
 
         console.log(`Computer's choice:- ${computerSelection}`);
         result = `It's a tie, there's still chances remaining`;
+        console.log(`Score: Player = ${playerScore}, Computer = ${computerScore}`);
     }
 
 
     else if ((toLowerP1ayerSelection === `rock` && computerSelection === `paper`) ||
-             (toLowerP1ayerSelection === `scissors` && computerSelection === `rock`) ||
-             (toLowerP1ayerSelection === `paper` && computerSelection === `scissors`)) {
-       
-     console.log(`Computer's choice:- ${computerSelection}`);
-     result = `You Lose! ${computerSelection} beats ${toLowerP1ayerSelection}`;
+        (toLowerP1ayerSelection === `scissors` && computerSelection === `rock`) ||
+        (toLowerP1ayerSelection === `paper` && computerSelection === `scissors`)) {
+
+        console.log(`Computer's choice:- ${computerSelection}`);
+        result = `You Lose! ${computerSelection} beats ${toLowerP1ayerSelection}`;
+        console.log(`Score: Player = ${playerScore}, Computer = ${computerScore += 1}`);
     }
 
 
@@ -66,11 +73,32 @@ function playRound() {
         result = `Enter a valid option!!`;
     }
 
-    console.log(toLowerP1ayerSelection);
-
     return result;
+
 }
 
+//Function to start the game
+// This functions calls the playRound() function to start the game and end it when either the player or the computer reaches the score of 5
+function game() {
 
+    //for loop that calls the playRound() function and breaks when either of them reaches the score of 5 
+    for (; ;) {
+        console.log(playRound());
+        if(computerScore === 5 || playerScore === 5) break;
+    }
+    
+    //when computer's score reaches 5 this resets the both scores to 0 and displays the result
+    if(computerScore==5){
+        playerScore = 0;
+        computerScore = 0;
+        return result=`Too bad you lost this game`;
+    }
 
+    //when player's score reaches 5 this resets the both scores to 0 and displays the result
+    else if(playerScore===5){
+        playerScore = 0;
+        computerScore = 0;
+        return result=`Congratulations! You won!`;
+    }
 
+} 
